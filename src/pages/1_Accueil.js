a49ddc4334078c80b91898b3b33ede86151a42bb
Tom Tournillon <tom.tournillon@gmail.com>
2023-10-22 20:55:56 +0200

    Texte

--- src/pages/1_Accueil.js
+++ src/pages/1_Accueil.js
@@ -3,7 +3,11 @@ import React from 'react';
 export default function Accueil() {
     return (
         <div>
-            <p>hello</p>
+            <h1>Bienvenue</h1>
+            <p>Je suis Mathilde, photographe professionnelle spécialisée dans la maternité, les nouveau-nés et les bébés... J'aime également photographier les couples et vos familles.</p>
+            <p>J'aime la vie, j'aime mon métier, et je le fais avec amour et passion ! Maintenant vous en savez un peu plus sur moi, je vous laisse découvrir mon travail...</p>
+            <p>Au bout de cette superbe année nous sommes rentrés en France et nous avons repris notre vie normale et notre boulot. Retour à la réalité ! Autant vous dire que psychologiquement ça n’a pas été facile…</p>
+            <p>Heureusement, quelques mois après notre retour, nous apprenons une merveilleuse nouvelle, un petit bébé était en train de grandir dans mon ventre. Une petite princesse a vu le jour 9 mois après. Une nouvelle aventure qui démarrait, encore plus extraordinaire... Devenir Maman ! Ce nouveau rôle qui a complètement boulversé ma vie ! La naissance d’un enfant c’est vraiment quelque chose d’indescriptible, un nouveau sentiment qui vous inonde d’amour... Et 2 ans et demi après une seconde merveille pointe le bout de son nez, pour ajouter encore plus d’amour à notre petite famille... Maman et femme comblée, c’est à ce moment là que je décide de changer de métier et d’allier deux choses que j’aimais, la photo et les bébés ! Après pas mal de temps passé à me former et de travail acharné, je suis devenue photographe professionnelle et je me suis spécialisée dans la prise de vue artistique de la femme enceinte, des nouveau-nés et des bébés.</p>
         </div>
     )
 }
--- src/pages/2_Grossesse.js
+++ src/pages/2_Grossesse.js
@@ -3,7 +3,15 @@ import React from 'react';
 export default function Grossesse() {
     return (
         <div>
-            <p>grossesse</p>
+            <h1>Séance grossesse</h1>
+            <p>L'idéal pour réaliser cette séance est au cours du 7ème mois, le ventre est bien rond, maman pas trop fatiguée et il y a aussi beaucoup moins de risques de rater sa séance si bébé arrive un peu avant... Les photos peuvent se dérouler en intérieur ou extérieur au choix, et on discutera avant du thème, style, tenues, lieux... Elles peuvent se faire seule, en couple ou en famille.</p>
+            <p>N'ayez aucune crainte, l'ambiance est détendue ! Je suis là pour vous mettre à l'aise et vous guider de A à Z sur les poses, le regard, l'expression du visage...</p>
+            <p>Mon métier c'est vous mettre en valeur vous et vos jolies courbes pour créer ces superbes souvenirs si importants dans la vie d'une femme.</p>
+            <p>Afin de vous sentir encore plus belle que vous ne l'êtes déjà, je mets à disposition des mamans un large choix de tenues, accessoires et jolies robes de grossesse que vous pourrez choisir avant la séance ; encore une fois, je serai là pour vous conseiller. J'ai également quelques robes assorties à maman pour les petites filles. Vous pouvez aussi amener des tenues ou accessoires personnels, si vous le souhaitez !</p>
+            <p>Parce qu'elle est propre à chacune, je m'adapterai aussi à votre pudeur... Je réalise avec les femmes enceintes des photos avec de superbes robes et voilages mais aussi des photos un peu plus dénudées, ou du totalement nu pour un rendu très naturel et pur, vous pouvez me faire part de vos envies ou préférences pendant nos échanges.</p>
+            <p>Une fois le rendez-vous pris, je vous détaillerai par message quels sous-vêtements mettre suivant les tenues choisies, et comment habiller les papas et garçons.</p>
+            <p>Pour avoir des disponibilités, les réservations se font dès la fin de votre 1er trimestre. Un acompte vous sera demandé pour valider votre réservation.</p>
+            <p>Voilà pour les points importants, si vous avez d'autres questions surtout n'hésitez pas à revenir vers moi !</p>
         </div>
     )
 }
