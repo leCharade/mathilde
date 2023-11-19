3ce9cc19239b6e1177565ad7e85c115517d35acf
Tom Tournillon <tom.tournillon@gmail.com>
2023-11-19 14:50:50 +0100

    Page d'accueil

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,22 +4,26 @@ import logo from '../assets/logo.png'
 
 export default function Header() {
     return (
-        <div className="background-block background-block__header">
-            <header>
-                <img className="logo" src={logo} alt="Logo du site. Il y est écrit « Un jour, une histoire... » en style calligraphique, suivi en-dessous par « Photographie by Mathilde »."/>
-                <nav>
-                    <ul>
-                        <li><NavLink to="/">Accueil</NavLink></li>
-                        <li><NavLink to="/grossesse">Grossesse</NavLink></li>
-                        <li><NavLink to="/nouveau-nes">Nouveau-nés</NavLink></li>
-                        <li><NavLink to="/enfants-bebe">Enfants bébés</NavLink></li>
-                        <li><NavLink to="/1er-anniversaire">1er anniversaire</NavLink></li>
-                        <li><NavLink to="/famille">Famille</NavLink></li>
-                        <li><NavLink to="/tarifs">Tarifs</NavLink></li>
-                        <li><NavLink to="/contact">Contact</NavLink></li>
-                    </ul>
-                </nav>
-            </header>
-        </div>
+        <>
+            <div className="background-block background-block__header">
+                <header>
+                    <img className="logo" src={logo} alt="Logo du site. Il y est écrit « Un jour, une histoire... » en style calligraphique, suivi en-dessous par « Photographie by Mathilde »."/>
+                    <nav>
+                        <ul>
+                            <li><NavLink to="/">Accueil</NavLink></li>
+                            <li><NavLink to="/grossesse">Grossesse</NavLink></li>
+                            <li><NavLink to="/nouveau-nes">Nouveau-nés</NavLink></li>
+                            <li><NavLink to="/enfants-bebe">Enfants bébés</NavLink></li>
+                            <li><NavLink to="/1er-anniversaire">1er anniversaire</NavLink></li>
+                            <li><NavLink to="/famille">Famille</NavLink></li>
+                            <li><NavLink to="/tarifs">Tarifs</NavLink></li>
+                            <li><NavLink to="/contact">Contact</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+            </div>
+            <div className="background-block background-block__margin-header">
+            </div>
+        </>
     )
 }
