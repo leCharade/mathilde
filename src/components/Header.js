be706b7670ffff113e21736ec34d134df01afbdd
Tom Tournillon <tom.tournillon@gmail.com>
2023-11-28 14:34:41 +0100

    Responsive : width réglée

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
     return (
@@ -8,6 +10,9 @@ export default function Header() {
             <div className="background-block background-block__header">
                 <header>
                     <img className="logo" src={logo} alt="Logo du site. Il y est écrit « Un jour, une histoire... » en style calligraphique, suivi en-dessous par « Photographie by Mathilde »."/>
+                    <div className="menu-icon">
+                        <FontAwesomeIcon icon={faBars} />
+                    </div>
                     <nav>
                         <ul>
                             <li><NavLink to="/">Accueil</NavLink></li>
