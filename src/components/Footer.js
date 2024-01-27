fd9ef1b5666883282cd9177064b867009f7d5086
Tom Tournillon <tom.tournillon@gmail.com>
2024-01-27 15:50:08 +0100

    maj footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,14 @@ export default function Footer() {
     return (
         <div className="background-block background-block__footer">
             <footer>
-                <div>Copyright 2023 - Réalisé par Tom Tournillon. Tous droits réservés.</div>
-                <div>
+                <div>Copyright 2024 - Réalisé par Tom Tournillon. Tous droits réservés.</div>
+                {/* <div>
                     <ul className="links">
                         <li>Mentions légales</li>
                         <li>Politique de confidentialité</li>
                         <li>Politique des cookies</li>
                     </ul>
-                </div>
+                </div> */}
             </footer>
         </div>
     )
