1e3d05b73dd9ea13fa9a7cb969b5133d8bdc3b66
Tom Tournillon <tom.tournillon@gmail.com>
2024-02-03 16:49:29 +0100

    Test API 5

--- back/controllers/contact.js
+++ back/controllers/contact.js
@@ -28,11 +28,95 @@ exports.getMail = (req, res) => {
         message: req.body.message
     }
 
+    let mailSubject = "";
+    switch (mail.objet) {
+        case "reserver":
+            mailSubject = "[Réserver une séance] "
+            switch (mail.seance) {
+                case "grossesse":
+                    mailSubject += "Séance grossesse"
+                    break;
+                case "nouveau-nes":
+                    mailSubject += "Séance nouveau-nés"
+                    break;
+                case "forfait": 
+                    mailSubject += "Forfait grossesse + nouveau-nés"
+                    break;
+                case "enfants-bebes": 
+                    mailSubject += "Séance enfants / bébés"
+                    break;
+                case "1er-anniversaire":
+                    mailSubject += "Séance 1er anniversaire"
+                    break;
+                case "famille":
+                    mailSubject += "Séance famille"
+                    break;
+                default:
+                    res.status(401).json({ message: "Veuillez renseigner une séance !" })
+            }
+            break;
+        case "info":
+            mailSubject = "[Demande d'informations] "
+            switch (mail.seance) {
+                case "grossesse":
+                    mailSubject += "Séance grossesse"
+                    break;
+                case "nouveau-nes":
+                    mailSubject += "Séance nouveau-nés"
+                    break;
+                case "forfait": 
+                    mailSubject += "Forfait grossesse + nouveau-nés"
+                    break;
+                case "enfants-bebes": 
+                    mailSubject += "Séance enfants / bébés"
+                    break;
+                case "1er-anniversaire":
+                    mailSubject += "Séance 1er anniversaire"
+                    break;
+                case "famille":
+                    mailSubject += "Séance famille"
+                    break;
+                case "autre":
+                    mailSubject += "Autre"
+                    break;
+                default:
+                    res.status(401).json({ message: "Veuillez renseigner une séance !" })
+            }
+            break;
+        case "partenariat":
+            mailSubject = "[Partenariat commercial] " + mail.motif
+            break;
+        case "autre":
+            mailSubject = "[Autre] " + mail.motif
+            break;
+        default:
+            res.status(401).json({ message: "Veuillez renseigner un motif de demande !" })  
+    }
+    if (mail.nom === "") {
+        res.status(401).json({ message: "Veuillez renseigner un nom de famille !" })  
+    }
+    if (mail.prenom === "") {
+        res.status(401).json({ message: "Veuillez renseigner un prénom !" })  
+    }
+    if (mail.telephone === "" && mail.email === "") {
+        res.status(401).json({ message: "Veuillez renseigner une méthode de contact !" })  
+    }
+    if (mail.message === "") {
+        res.status(401).json({ message: "Veuillez écrire un message !" })  
+    }
+
+    const mailMessage = '<p><strong>Nom :</strong> ' + mail.nom + '</p>' +
+    '<p><strong>Prénom :</strong> ' + mail.prenom + '</p>' +
+    '<p><strong>Téléphone :</strong> ' + mail.telephone + '</p>' +
+    '<p><strong>Adresse email :</strong> ' + mail.email + '</p>' +
+    '<p><strong>Motif de la demande : ' + mailSubject + '</strong></p><br />' +
+    '<p><strong>Message :</strong></p><p>' + mail.nom + '</p>'
+
     const mailOptions = {
         from:'noreply@photographiebymathilde.fr',
         to:'tom.tournillon@gmail.com',
-        subject:mail.motif,
-        text:mail.message
+        subject:mailSubject,
+        text:mailMessage
     }
 
     transporter.sendMail(mailOptions, function(error, info) {
